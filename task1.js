3a072ec1b38b9998957a0916734a32625bfa233a
Pravalika22 <pravalika14325@gmail.com>
2021-09-29 01:22:29 +0530

    Initial commit

---
+++ task1.js
@@ -0,0 +1,26 @@
+
+
+function myFunction() {
+    alert("You selected some text!");
+}
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+function myFunction() {
+    var x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+  }
+  function WhichButton(event) {
+    alert("You pressed button: " + event.button)
+  }
+
+  function myFunction() {
+    var x = document.getElementById("mySelect").value;
+    document.getElementById("demo").innerHTML = "You selected: " + x;
+  }
+
+  function myFunction() {
+    var x = document.getElementById("mySelect").value;
+    document.getElementById("demo").innerHTML = "You selected: " + x;
+  }
+  
